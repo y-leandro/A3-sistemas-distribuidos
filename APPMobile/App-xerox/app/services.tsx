e92a8ea568fc4bf626081117aa175443a3d3a5c4
Leandro de Medeiros Filho <102531267+y-leandro@users.noreply.github.com>
2026-03-04 09:05:04 -0300

    Delete APPMobile directory

--- APPMobile/App-xerox/app/services.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Services() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Serviços</Text>
-    </View>
-  );
-}
--- APPMobile/App-xerox/scr/components/screen.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-
-export default function Screen({ children }: any) {
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#5A9DBA" }}>
-            {children}
-        </SafeAreaView>
-    );
-}
